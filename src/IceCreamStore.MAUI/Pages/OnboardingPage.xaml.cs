37973dfb8c146839f266ce165ac9abaf00531b59
Дмитрий Горбунов <bous07@mail.ru>
2024-06-10 20:59:15 +0300

    Temp change 39:28,1

--- src/IceCreamStore.MAUI/Pages/OnboardingPage.xaml.cs
+++ src/IceCreamStore.MAUI/Pages/OnboardingPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class OnboardingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
 }
