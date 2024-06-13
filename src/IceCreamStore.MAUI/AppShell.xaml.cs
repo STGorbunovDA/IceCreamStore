f895d9d5fb9e80aaa8422f5cb595e023e2bd755a
Дмитрий Горбунов <bous07@mail.ru>
2024-06-13 20:50:10 +0300

    Temp change 1:09:18

--- src/IceCreamStore.MAUI/AppShell.xaml.cs
+++ src/IceCreamStore.MAUI/AppShell.xaml.cs
@@ -1,10 +1,34 @@
-﻿namespace IceCreamStore.MAUI
+﻿using IceCreamStore.MAUI.Pages;
+
+namespace IceCreamStore.MAUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            // Routing.RegisterRoute(nameof(SigninPage), typeof(SigninPage));
+            // Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
+
+            RegisterRoutes();
+        }
+
+        private readonly static Type[] _routablePageTypes =
+            [
+                typeof(SigninPage),
+                typeof(SignupPage),
+                typeof(MyOrdersPage),
+                typeof(OrderDetailsPage),
+                typeof(DetailsPage),
+            ];
+
+        private static void RegisterRoutes()
+        {
+            foreach (var pageType in _routablePageTypes)
+            {
+                Routing.RegisterRoute(pageType.Name, pageType);
+            }
         }
     }
 }
--- src/IceCreamStore.MAUI/Pages/OnboardingPage.xaml.cs
+++ src/IceCreamStore.MAUI/Pages/OnboardingPage.xaml.cs
@@ -11,4 +11,14 @@ public partial class OnboardingPage : ContentPage
     {
 		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
     }
+
+    private async void Signin_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SigninPage));
+    }
+
+    private async void Signup_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SignupPage));
+    }
 }
